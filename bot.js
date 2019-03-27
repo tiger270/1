0fec195d8b6af5eb0570d4b87c8a8b1e933f0d53
tiger270 <47055184+tiger270@users.noreply.github.com>
2019-03-27 12:56:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
-var adminprefix = 'Y'
+var adminprefix = 'Y1'
 
 
 const developers = ["541322693178425358"]
