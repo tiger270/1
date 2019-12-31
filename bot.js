52ec1ac38eb410e282cd26855422aff4481e41ea
dLqx <kontexxdz@gmail.com>
2019-12-31 17:24:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,37 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
-var adminprefix = 'Y1'
+var prefix = "$";
+var adminprefix = 'Y2'
+console.log(`Your Bot Is Online `);
 
 
-const developers = ["541322693178425358"]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/LionDev");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
+
+client.on("message", message => {
+    if (message.content.startsWith(prefix + 'bc')) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n`);
+  })
+  message.channel.send(`**${message.guild.members.filter( m => m.presence.status !== 'all').size} Recived **`);
+  message.delete();
+  };
+  });
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "obc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n`);
+  })
+   message.channel.send(`**${message.guild.members.filter(m => m.presence.status !== 'online').size} Recived **`); 
+   message.delete(); 
+  };     
+  });
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
